b979eb09f510eb2601c62d107de3747b2800bcd4
diegopq <d.padillaquijas@gmail.com>
2023-08-09 21:41:41 -0600

    signup base logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup(authDto: AuthDto) {
-    console.log(authDto);
-    return { message: 'This is a signup route' };
+  async signup(authDto: AuthDto) {
+    //generate hash
+    const hash = await argon.hash(authDto.password);
+
+    //save user to db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: authDto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
