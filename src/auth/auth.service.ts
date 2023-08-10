30094ae5487aa07a8e4f71a72da356edcef67fca
diegopq <d.padillaquijas@gmail.com>
2023-08-09 21:07:03 -0600

    auth dto creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
+  signup(authDto: AuthDto) {
+    console.log(authDto);
     return { message: 'This is a signup route' };
   }
 
